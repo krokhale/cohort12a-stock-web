68a3ad2e25bbf8f8d647ab31b241cf36ece0b01f
krokhale <krishna.rokhale@gmail.com>
2021-04-05 19:17:45 -0700

    checking in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 1. Get the look and feel of the Stock name and BUY and SELL buttons going
+// 2. Work on the Table UI
+
+// Next Session
+
+// 1. Add database to the backend server
+// 2. Add api router to the backend server
+// 3. Define the models in the system
+// 4. Test out the API in a REST client like postman
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={'grid grid-cols-12'}>
+          <div className={'col-span-12 border p-3'}>
+              <h1 className={'text-2xl text-center font-bold'}>Paper Trader</h1>
+          </div>
+
+          <div className={'col-span-12 md:col-span-6 border p-5'}>
+              <h1 className={'text-3xl font-bold'}>Box 1</h1>
+
+              <div className={'grid grid-cols-12'}>
+                  <div className={'col-span-12 md:col-span-6 border p-3'}>Box 3</div>
+                  <div className={'col-span-12 md:col-span-6 hidden md:block border p-3'}>Box 4</div>
+                  <div className={'col-span-12 md:col-span-6 border p-3'}>Box 5</div>
+                  <div className={'col-span-12 md:col-span-6 border p-3'}>Box 6</div>
+                  <div className={'col-span-12 border p-3'}>Box 7</div>
+              </div>
+
+          </div>
+
+          <div className={'col-span-12 md:col-span-6 border p-5'}>
+              <h1 className={'text-3xl font-bold'}>Box 2</h1>
+
+              <table className={'border w-full'}>
+                  <tr className={'border'}>
+                      <th>Firstname</th>
+                      <th>Lastname</th>
+                      <th>Age</th>
+                  </tr>
+                  <tr className={'border'}>
+                      <td>Jill</td>
+                      <td>Smith</td>
+                      <td>50</td>
+                  </tr>
+                  <tr className={'border'}>
+                      <td>Eve</td>
+                      <td>Jackson</td>
+                      <td>94</td>
+                  </tr>
+              </table>
+
+
+
+          </div>
+
+      </div>
+    </>
   );
 }
 
